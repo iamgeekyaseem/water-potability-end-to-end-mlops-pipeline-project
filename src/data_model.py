f24f9bd14dbae4e24bb3e1a88496cd5c1b0b8541
Aseem Gupta <aseem@Aseems-MacBook-Air.local>
2026-03-24 22:21:24 +0530

    fastAPI integration

---
+++ src/data_model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+import pickle
+
+class Water(BaseModel):
+    ph: float
+    Hardness: float
+    Solids: float
+    Chloramines: float
+    Sulfate: float
+    Conductivity: float
+    Organic_carbon: float
+    Trihalomethanes: float
+    Turbidity: float
---
+++ src/main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pickle
+from fastapi import FastAPI
+
+from data_model import Water
+
+app = FastAPI(
+    title="Water Potability Prediction API",
+    description="An API to predict water potability based on various features.",
+    version="1.0.0"
+)
+
+@app.get("/")
+def index():
+    return {"message": "Welcome to the Water Potability Prediction API!"}
+
+with open(r'/Users/aseem/projects/mlpipeline/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+@app.post("/predict")
+def predict(water: Water):
+    input_data = pd.DataFrame({
+        "ph": [water.ph],
+        "Hardness": [water.Hardness],
+        "Solids": [water.Solids],
+        "Chloramines": [water.Chloramines],
+        "Sulfate": [water.Sulfate],
+        "Conductivity": [water.Conductivity],
+        "Organic_carbon": [water.Organic_carbon],
+        "Trihalomethanes": [water.Trihalomethanes],
+        "Turbidity": [water.Turbidity]
+    })
+    prediction = model.predict(input_data)
+    potability = "Potable" if prediction[0] == 1 else "Not Potable"
+    
+    return {"prediction": potability}
+    
